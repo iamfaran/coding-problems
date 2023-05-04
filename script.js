ad9c6e133ac175c6992b5788333ef57beb238cfe
Faran Javed <faran1997@outlook.com>
2023-05-04 11:52:17 +0500

    understood validParens

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello");
+// () => true
+// ( => false
+// ) => false
+// (()) => true
+// (() => false
+// (())() => true
+// (()()) => true
+// (()() => false
+//(())((()())()) =>  true
+
+function checkParenthesis(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "(") count++;
+    if (str[i] === ")") count--;
+    if (count < 0) return false;
+  }
+  return count === 0;
+}
